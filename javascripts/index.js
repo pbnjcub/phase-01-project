6f39c0268d3b23f68e4951a2db9ac73b3d600289
Paul Jung <pbnjcub@gmail.com>
2022-08-12 14:08:57 -0400

    tested data retrieval from local json

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,12 +1,6 @@
 // This is for our javascript logic
-let heroes = [
-    {
-        name: 'Captain America',
-        description: 'America\'s first Avenger',
-        powers: 'super-human strength and amazing leadership abilities',
-
-    }
-]
+let heroes = []
+const baseUrl = 'http://localhost:3000'
 
 
 //node getters
@@ -16,14 +10,22 @@ const browseLink = () => document.getElementById('browseHero')
 const homeLink = () => document.getElementById('home')
 
 //functions
+
+const fetchHeroes = () =>
+    fetch(baseUrl + "/heroes")
+    .then(resp => resp.json())
+    .then(data => {
+        heroes = data
+    })
+
 const renderForm = () => {
     // create a form
 
 }
 
-const renderHero = () => {
+const renderHero = (hero) => {
     const col = document.createElement('div')
-    col.className = 'col s12 m7'
+    col.className = 'col s12 m4 l3'
     
     col.appendChild(createCard(hero))
 
@@ -49,7 +51,7 @@ const renderFormPage = (e) => {
     e.preventDefault()
 
     resetMain()
-    
+
     const h3 = document.createElement('h3')
     h3.innerText = 'Find Your Hero'
 
@@ -64,7 +66,6 @@ const renderBrowsePage = (e) => {
     e.preventDefault()
 
     resetMain()
-    alert('hello')
     const h3 = document.createElement('h3')
     h3.innerText = 'Browse Available Heroes'
 
@@ -78,14 +79,21 @@ const renderHomePage = (e) => {
     e.preventDefault()
 
     resetMain()
-    
+
     const h3 = document.createElement('h3')
-    const p = document.createElement('p')
+    const p1 = document.createElement('p')
+    const p2 = document.createElement('p')
+    const p3 = document.createElement('p')
+    const br = document.createElement('br')
     h3.innerText = 'Welcome to myHERO'
-    p.innerText = 'If you are interested in getting to know heroes who have similar interests and powers to your own, this is the place to be. <br> Click on the "Find Your Hero" link to take the "Personality Quiz" to narrow your search. <br> Or you can browse through all our heroes using the "Browse Heroes" tool.'
+    p1.innerText = 'If you are interested in getting to know heroes who have similar interests and powers to your own, this is the place to be.'
+    p2.innerText = 'Click on the "Find Your Hero" link to take the "Personality Quiz" to narrow your search.' 
+    p3.innerText = 'Or you can browse through all our heroes using the "Browse Heroes" tool.'
 
     mainDiv().appendChild(h3)
-    mainDiv().appendChild(p)
+    mainDiv().appendChild(p1)
+    mainDiv().appendChild(p2)
+    mainDiv().appendChild(p3)
 }
 
 //event listeners
@@ -102,11 +110,14 @@ const renderHomeEvent = () => {
 }
 
 const browseHeroEvent = () => {
-    browseLink().addEventlistener('click', renderBrowsePage)
+    browseLink().addEventListener('click', renderBrowsePage)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    fetchHeroes()
     attachFormEvent()
+    renderHomeEvent()
+    browseHeroEvent()
 })
 
 //helpers
@@ -119,24 +130,25 @@ const createCard = (hero) => {
     const pDescription = document.createElement('p')
     const divCardAction = document.createElement('div')
     const linkOne = document.createElement('a')
+    const img = document.createElement('img')
 
     divCard.className = 'card'
     divImage.className = 'card-image'
     span.className = 'card-title'
-    divCardContent = 'card-content'
-    divCardAction = 'card-action'
+    divCardContent.className = 'card-content'
+    divCardAction.className = 'card-action'
 
-    link1.setAttribute('href','')
-    img.setAttribute('src',"https://www.ed92.org/wp-content/uploads/2021/06/captain-america-1-657x1024.png")
+    linkOne.setAttribute('href','')
+    img.setAttribute('src', hero.imageUrl)
 
-    span.innerText = 'Card Title'
-    pDescription.innerText = 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.'
+    span.innerText = hero.name
+    pDescription.innerText = hero.description
     linkOne.innerText = 'more'
     
     divImage.appendChild(img)
     divImage.appendChild(span)
     divCardContent.appendChild(pDescription)
-    divCardAction.appendChild(link1)
+    divCardAction.appendChild(linkOne)
 
     divCard.appendChild(divImage)
     divCard.appendChild(divCardContent)
