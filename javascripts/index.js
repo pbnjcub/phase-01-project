f39524a94eb6e9ce8c4bddaf8fdd2f3673e52122
Paul Jung <pbnjcub@gmail.com>
2022-08-12 11:35:47 -0400

    created click event

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,19 @@
 // This is for our javascript logic
+
+//node getters
+const formLink = () => document.getElementById('findHero')
+
+//event handlers
+const renderFormPage = (e) => {
+    e.preventDefault()
+    alert('hi')
+}
+
+//event listeners
+const attachFormEvent = () => {
+    formLink().addEventListener('click', renderFormPage)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    attachFormEvent()
+})
